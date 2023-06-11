102fb2dd31401f7bc0697c5a4ae690fde40d3ea3
Arvind <thakurarvindkr10@gmail.com>
2023-06-12 00:06:52 +0530

    Banner and images added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Banner from './components/Banner.jsx'
 
 const App = () => {
     return (
         <div>
-            <Navbar/>
+            <Navbar />
+            <Banner/>
         </div>
     )
 }
---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import banner from '../images/1193007.jpg'
+const Banner = () => {
+    return (
+        <div className={`bg-[url(https://images7.alphacoders.com/127/thumbbig-1271368.webp)] h-[60vh] bg-center bg-cover flex items-end justify-center`}>
+            <div className='text-5xl text-white  p-4 bg-gray-900 w-full flex justify-center bg-opacity-50'>Spider-Man : No Way Home</div>
+        </div >
+    )
+}
+
+export default Banner
