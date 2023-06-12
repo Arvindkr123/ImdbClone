75412702c30d540af6eca360dbb87b5bb03e57c9
Arvind <thakurarvindkr10@gmail.com>
2023-06-12 17:11:45 +0530

    added button in fav component

--- src/components/Fav.jsx
+++ src/components/Fav.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Fav = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className='flex justify-center space-x-2 mt-4 px-2 flex-wrap'>
+                <button className='text-lg m-2 p-1 px-2 font-bold bg-blue-400 text-white rounded-xl'>All Genres</button>
+                <button className='text-lg m-2 hover:bg-gray-400 duration-300  p-1 px-2 font-bold bg-blue-400 text-white rounded-xl'>All Genres</button>
+                <button className='text-lg m-2 hover:bg-gray-400 duration-300  p-1 px-2 font-bold bg-blue-400 text-white rounded-xl'>All Genres</button>
+            </div>
+            <div>Input Container</div>
+            <div>Table Container</div>
+            <div>Pagination</div>
+        </>
     )
 }
 
