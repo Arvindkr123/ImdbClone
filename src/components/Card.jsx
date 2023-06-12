840d5c2be83581cbcd6be6381ce8847bea16ed56
Arvind <thakurarvindkr10@gmail.com>
2023-06-12 10:20:11 +0530

    added string truncateString

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
 const Card = ({ movie }) => {
+    const truncateString = (str, maxLength) => {
+        if (str.length > maxLength) {
+            return str.substring(0, maxLength) + '...';
+        } else {
+            return str;
+        }
+    };
     return (
         <div className='p-1 hover:scale-110 duration-300 mt-8'>
             <img src={`https:/image.tmdb.org/t/p/w500/${movie.backdrop_path}`} className='bg-cover bg-center rounded-t-lg' />
-            <div className='w-full bg-gray-900 text-white py-2 text-center text-sm md:text-xl rounded-b-lg'>{movie.title}</div>
+            <div className='w-full'>
+                <div className='text-center bg-gray-900 text-white py-2 text-sm md:text-xl rounded-b-lg '>{truncateString(movie.title, 20)}</div>
+            </div>
         </div>
     )
 }
