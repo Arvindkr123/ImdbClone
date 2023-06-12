70c6f5318afa35561d8eb1c859eb0e6d2c665916
Arvind <thakurarvindkr10@gmail.com>
2023-06-12 08:43:05 +0530

    added loader spinner

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from './Card'
+import axios from 'axios';
+import { Audio, Oval } from 'react-loader-spinner'
 
 const Movies = () => {
+    const url = 'https://api.themoviedb.org/3/trending/movie/week?api_key=9c3e3681cf1cc02a6205b0db0f1466cd&page=1';
+    const [movies, setMovies] = useState([]);
+    const getMoviesData = async () => {
+        let res = await axios.get(url)
+        console.table(res.data.results);
+        setMovies(res.data.results)
+    }
+    useEffect(() => {
+        getMoviesData();
+    }, [])
     return (
         <div className='mb-8'>
             <div className='mt-8 text-xl md:text-2xl text-bold text-center'>Trending Movies</div>
-            <div className='flex flex-wrap justify-center'>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-            </div>
+            {
+                movies.length == 0 ?
+                    <div className='flex justify-center items-center m-8'>
+                        <Oval
+                            ariaLabel="loading-indicator"
+                            height={100}
+                            width={100}
+                            strokeWidth={5}
+                            strokeWidthSecondary={1}
+                            color="blue"
+                            secondaryColor="white"
+                        />
+                    </div>
+                    : <div className='flex flex-wrap justify-center'>
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                    </div>
+            }
         </div>
     )
 }
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Pagination = () => {
+    const [pageNumber, setPageNumber] = useState(1);
+    const gobehind = () => {
+        if (pageNumber > 1) {
+            setPageNumber(pageNumber - 1)
+        }
+    }
     return (
         <div className='w-full flex justify-center mb-8'>
-            <button className='p-2 border-2 border-indigo-500 text-indigo-500 border-r-0 rounded-l-xl'>Previous</button>
-            <button className='p-2 border-2 border-indigo-500 text-indigo-500 bg-gray-100'>1</button>
-            <button className='p-2 border-2 border-indigo-500 text-indigo-500 border-l-0 rounded-r-xl'>Next</button>
+            <button className='p-2 border-2 border-indigo-500 text-indigo-500 border-r-0 rounded-l-xl' onClick={gobehind}>Prev</button>
+            <button className='p-2 border-2 border-indigo-500 text-indigo-500 bg-gray-100'>
+                {pageNumber}
+            </button>
+            <button className='p-2 border-2 border-indigo-500 text-indigo-500 border-l-0 rounded-r-xl' onClick={() => setPageNumber(pageNumber + 1)}>Next</button>
         </div>
     )
 }
